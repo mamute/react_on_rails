48b54fef600088d4e1a8fac313e1c8dbd368e55f
Marcus Sá <marcus.m.sa@gmail.com>
2017-09-26 10:28:47 -0300

    Fix rubocop issues added in 0.50.0

--- app/helpers/react_on_rails_helper.rb
+++ app/helpers/react_on_rails_helper.rb
@@ -177,7 +177,7 @@ module ReactOnRailsHelper
   # that contains a data props.
   def redux_store_hydration_data
     return if @registered_stores_defer_render.blank?
-    @registered_stores_defer_render.reduce("".dup) do |accum, redux_store_data|
+    @registered_stores_defer_render.reduce(+"") do |accum, redux_store_data|
       accum << render_redux_store_data(redux_store_data)
     end.html_safe
   end
@@ -413,11 +413,11 @@ module ReactOnRailsHelper
 
   def initialize_redux_stores
     return "" unless @registered_stores.present? || @registered_stores_defer_render.present?
-    declarations = "var reduxProps, store, storeGenerator;\n".dup
+    declarations = +"var reduxProps, store, storeGenerator;\n"
 
     all_stores = (@registered_stores || []) + (@registered_stores_defer_render || [])
 
-    result = <<-JS.dup
+    result = +<<-JS
       ReactOnRails.clearHydratedStores();
     JS
 
